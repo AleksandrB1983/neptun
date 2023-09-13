562ecd6bae7ca6d5229dba46a6bfc9d0aa83789c
Aleksandr Belousov <lksndrbelousov1983@mail.ru>
2023-09-13 17:00:49 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 24350; // стоимость авиабилета
+        int bonus = 20; // стоимость одного бонуса
+
+        System.out.println("Количество бонусов из стоимости билета:");
+        System.out.println((price / bonus) + " количество бонусов");
+
+
+    }
+}
